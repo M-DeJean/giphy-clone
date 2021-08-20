2dbcdba41e65706bd5f1dac4ed2bf3b3e96f725e
M-DeJean <dejeantech@yahoo.com>
2021-08-20 14:24:49 -0700

    Updates

---
+++ precache-manifest.e7ad8913c45a7dd7d5b4a19223e68648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343031d96b4a2ad0da00f2902ad77cbb",
+    "url": "/giphy-clone/index.html"
+  },
+  {
+    "revision": "aeb89ab29c9b6d8b5a2d",
+    "url": "/giphy-clone/static/css/main.85a65b3a.chunk.css"
+  },
+  {
+    "revision": "c55daecf56d97acba9a8",
+    "url": "/giphy-clone/static/js/2.ba9a53e1.chunk.js"
+  },
+  {
+    "revision": "0cf5ba2c00bbc0b01089c885c528fd0c",
+    "url": "/giphy-clone/static/js/2.ba9a53e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeb89ab29c9b6d8b5a2d",
+    "url": "/giphy-clone/static/js/main.3f286929.chunk.js"
+  },
+  {
+    "revision": "78ab602955c5937e4bff",
+    "url": "/giphy-clone/static/js/runtime-main.3738d9bd.js"
+  }
+]);
